a390193ce3c25d4808de95613945e5f195e3a2b5
mitshell <michau.benoit@gmail.com>
2020-10-27 21:43:29 +0100

    fix url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="card",
       author="Benoit Michau",
       author_email="michau.benoit@gmail.com",
-      url="http://michau.benoit.free.fr/codes/smartcard/",
+      url="https://github.com/mitshell/card/",
       description="A library to manipulate smartcards used in telecommunications systems (SIM, USIM)",
       long_description=open("README.txt", "r").read(),
       version="0.3",
